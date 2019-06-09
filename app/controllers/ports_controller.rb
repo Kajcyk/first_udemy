9738e0f3a70eb0ac7633e0574e320b7864010aa5
kajcyk <kajcyk@gmail.com>
2019-06-09 20:23:23 +0200

    Integrate new and create actions in port

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -1,2 +1,27 @@
 class PortsController < ApplicationController
+	def index
+		@ports = Port.all 
+	end
+
+	def new
+		@port = Port.new
+	end
+
+	def create
+    	@port = Port.new(port_params)
+
+    	respond_to do |format|
+	      if @port.save
+	        format.html { redirect_to ports_path, notice: 'Portfolio was successfully created.' }
+	      else
+	        format.html { render :new }
+	      end
+    	end
+ 	 end
 end
+
+private
+
+ def portfolio_params
+ 	params.require(:port).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+ end
