2f69aa094e0da3f77f751be18b7b31efc6ff9fd9
kajcyk <kajcyk@gmail.com>
2019-06-09 18:18:17 +0200

    Integrated index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ports
   resources :portfolios
   get 'pages/home'
   get 'pages/about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_180342) do
+ActiveRecord::Schema.define(version: 2019_06_09_104609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2019_06_08_180342) do
   end
 
   create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ports", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.text "body"
